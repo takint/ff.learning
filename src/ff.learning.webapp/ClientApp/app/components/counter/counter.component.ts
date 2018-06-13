61a3392daa8a5f6c49f0311be4fcdd5d7ee61d1b
Jim Lee <tin.le@groovetechnology.com>
2018-06-13 17:24:39 +0700

    Update client side

--- src/ff.learning.webapp/ClientApp/app/components/counter/counter.component.ts
+++ src/ff.learning.webapp/ClientApp/app/components/counter/counter.component.ts
@@ -6,8 +6,33 @@ import { Component } from '@angular/core';
 })
 export class CounterComponent {
     public currentCount = 0;
+    public listeningStart: any;
+    private timer: any;
+
+    public hours: number = 0;
+    public minutes: number = 0;
+    public seconds: number = 0;
 
     public incrementCounter() {
         this.currentCount++;
+    }
+
+    public startTimer() {
+        //let _self = this;
+        //this.timer = setInterval(function () {
+        //    _self.currentCount++;
+        //}, 1000);
+        this.listeningStart = new Date().getTime();
+
+    }
+
+    public stopTimer() {
+        //clearInterval(this.timer);
+        let now = new Date().getTime();
+        let earn = now - this.listeningStart;
+
+        this.hours = Math.floor((earn % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        this.minutes = Math.floor((earn % (1000 * 60 * 60)) / (1000 * 60));
+        this.seconds = Math.floor((earn % (1000 * 60)) / 1000);
     }
 }
